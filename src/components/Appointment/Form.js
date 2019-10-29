06c90e88240b203f5d01ad477b8868a1497a9d34
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-28 20:58:20 -0700

    added reset to cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,31 +1,38 @@
 import React, { useState } from 'react';
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
+import { restElement } from '@babel/types';
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewerId, setInterviewer] = useState(props.interviewer || null);
-  console.log(setName)
-  console.log(name)
+  const cancel = () => {
+    reset()
+    props.onCancel()
+  }
+  const reset = () => {
+    setName("")
+    setInterviewer(null)
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()} >
           <input
             className="appointment__create-input text--semi-bold"
             type="text"
             placeholder="Enter Student Name"
             value={name}
             onChange={e => setName(e.target.value)}
-            
+
           />
-        </form>
+        </form> 
         <InterviewerList interviewers={props.interviewers} interviewer={interviewerId} setInterviewer={id => setInterviewer(id)} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
