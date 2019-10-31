43f89e9d65bd90af48bf3ad59a274ecdb23d4ebf
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-30 19:56:41 -0700

    added hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+/* 
+take in an initial mode
+set the mode state with the initial mode provided
+return an object with a property mode 
+*/
+function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial]);
+  function transition (newMode, replace) {
+    setMode(newMode)
+    if (replace === true) {
+      // history.pop()
+      // setHistory(history)
+      setHistory([initial]);
+    } else {
+      setHistory([...history, mode])
+    }
+  }
+  function back () { 
+    setMode(history.pop())
+  }
+  return { mode, transition, back }
+  
+}
+
+export {useVisualMode}
