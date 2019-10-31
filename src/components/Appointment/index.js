a264cb792e07c08bc781d0b83f9ee6a81e7880c9
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-30 21:08:23 -0700

    implemented logic from useVisualMode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,36 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show.js";
 import Empty from "components/Appointment/Empty.js";
+import Form from "components/Appointment/Form.js";
+import useVisualMode from "hooks/useVisualMode"
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
+const { mode, transition, back } = useVisualMode(
+  props.interview ? SHOW : EMPTY
+);
   return (
     <React.Fragment>
     <h4>{Header(props)}</h4>
-    <div>{(props.interview ? Show(props.interview): Empty(props))}</div>
+    <div>
+    {mode === EMPTY && <Empty onAdd={() => {transition(CREATE)}} />}
+    {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
+    {mode === CREATE && (
+      <Form 
+        interviewers={props.interviewers}
+        onCancel={() => {back(EMPTY)}}
+      />
+    )}
+    </div>
     </React.Fragment>
   );
 
