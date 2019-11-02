5b357837f5974d10dad2b9d69755c1a8155df0cd
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-01 17:43:02 -0700

    implemented ability to update spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -32,42 +32,52 @@ export default function useApplicationData() {
 
   const setDay = day => dispatch({type: 'SET_DAY', value: { ...state, day }})
 
+  function getSpotsForDay(oldstate, id, change) {
+    const spots = oldstate.days.map((indexDay) => {
+      if(oldstate.day === indexDay.name){
+        return {...indexDay, spots: indexDay.spots + change} 
+      }
+      return indexDay 
+    })
+    return spots;
+  }  
+
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get(`/api/days`)),
       Promise.resolve(axios.get(`/api/appointments`)),
       Promise.resolve(axios.get(`/api/interviewers`)),
     ]).then((all) => {
-      dispatch({type: 'SET_APPLICATION_DATA', value: {days: all[0].data, appointments: all[1].data, interviewers: all[2].data}})    });
+      dispatch({type: 'SET_APPLICATION_DATA', value: {...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}})    });
   }, []);
 
   function bookInterview(id, interview) {
     return axios.put(`/api/appointments/${id}`, {interview})
     .then(() => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    dispatch({type: 'SET_INTERVIEW', value: {...state, appointments}})
+      const appointments = {
+        ...state.appointments,
+        [id]: {
+          ...state.appointments[id],
+          interview: { ...interview }
+        }
+      };
+      const days = getSpotsForDay(state, id, -1 )
+      dispatch({type: 'SET_INTERVIEW', value: {...state, appointments, days}})
     });
   }
 
   function cancelInterview(id, interview) {
     return axios.delete(`/api/appointments/${id}`, interview)
     .then(() => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    dispatch({type: 'SET_INTERVIEW', value: {...state, appointments}})
+      const appointments = {
+        ...state.appointments,
+        [id]: {
+          ...state.appointments[id],
+          interview: null
+        }
+      };
+      const days = getSpotsForDay(state, id, +1 )
+      dispatch({type: 'SET_INTERVIEW', value: {...state, appointments, days}})
     });
   }
   return { cancelInterview, bookInterview, setDay, state }
