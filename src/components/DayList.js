a3c52efb9da6c5e7f29f4506301147e23b590dd0
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-05 18:23:43 -0800

    added data test and unique key

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,6 +5,8 @@ export default function DayList(props) {
   const days = props.days.map(day => {
     return (
       <DayListItem 
+        key={day.id}
+        id={day.id}
         name={day.name} 
         spots={day.spots} 
         selected={day.name === props.day}
